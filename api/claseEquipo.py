4cc92b916a11f8336a5e2e6628c1319ff1b3298a
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2023-11-07 14:27:44 -0300

    Update claseEquipo.py

--- api/claseEquipo.py
+++ api/claseEquipo.py
@@ -1,7 +1,12 @@
+# Importa la clase Grupo
 from claseGrupo import Grupo
 
 # Define una nueva clase llamada Equipo que hereda de la clase Grupo
+# Equipo es instanciada dentro de la clase JodApp
 class Equipo(Grupo):
+    # En el constructor se encuentra **kwargs 
+    # Es el parámetro que nos permite crear objetos a partir de pasarle un diccionario con todos los valores de los atributos
+    # Como la base de datos de Firebase guarda en formato diccionario, es una forma más sencilla de volver a crear los objetos
     def __init__(self, **kwargs):
         # Llama al constructor de la clase base Grupo utilizando kwargs
         super().__init__(**kwargs)
@@ -16,19 +21,24 @@ class Equipo(Grupo):
     def setDeporte(self, valor, firebase):
         if(firebase.editarAtributos("Equipos", self.getID(), {'deporte': valor})):
             self.__deporte = valor
-            
-    # Método de polimorfismo para establecer el nombre y actualizarlo en Firebase
+
+    # En los siguientes metodos Setter ocurre polimorfimo
+    # Debido a que ya fueron definidos en la clase Grupo
+    # Sin embargo, ahora se les pasa el tipo especifico ("Equipos")
+    # Para establecer el nombre y actualizarlo en Firebase
     def setNombre(self, valor, firebase):
         super().setNombre(valor, firebase, "Equipos")
         
-    # Método de polimorfismo para establecer un integrante y actualizarlo en Firebase
+    # Método donde ocurre polimorfismo, para establecer un integrante y actualizarlo en Firebase
     def setIntegrante(self, valor, firebase):
         super().setIntegrante(valor, firebase, "Equipos")
         
-    # Método de polimorfismo para mostrar la lista de integrantes del equipo
+    # En este metodo publico ocurre polimorfismo debido a que se vuelve a definir 
+    # Mostrando tambien los datos especificos de Equipo en formato horizontal
     def mostrarLista(self):
         return f"{super().mostrarLista()}Deporte: {self.__deporte}"
         
-    # Método de polimorfismo para mostrar información del equipo
+    # En este metodo publico ocurre polimorfismo debido a que se vuelve a definir 
+    # Mostrando tambien los datos especificos de Equipo
     def mostrar(self):
         return f"{super().mostrar()}Deporte: {self.__deporte}"
