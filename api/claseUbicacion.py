8976e0f91a21760ce801cef1e8da519a46d18a05
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2023-11-07 14:51:28 -0300

    Update claseUbicacion.py

--- api/claseUbicacion.py
+++ api/claseUbicacion.py
@@ -1,31 +1,35 @@
-
+# Se define la clase Ubicacion
+# Es una clase contenida por la clase Evento
+# Se realiza la instancia de esta clase al instanciar alguna de las hijas de Evento (Fiesta, Concierto o Match) dentro de la clase JodApp
 class Ubicacion():
     def __init__(self, latitud, longitud, descripcion):
         # Constructor de la clase que inicializa los atributos privados
+        # Latitud y Longitud guardan las coordenadas
         self.__latitud = latitud
         self.__longitud = longitud
+        # Contiene la descripcion de la ubicacion en formato cadena
         self.__descripcion = descripcion
 
-    # Método para obtener el valor de la latitud
+    # Método publico para obtener el valor de la latitud
     def getLatitud(self):
         return self.__latitud
         
-    # Método para obtener el valor de la longitud
+    # Método publico para obtener el valor de la longitud
     def getLongitud(self):
         return self.__longitud
         
-    # Método para obtener la descripción de la ubicación
+    # Método publico para obtener la descripción de la ubicación
     def getDescripcion(self):
         return self.__descripcion
         
-    # Método para modificar el valor de la latitud
+    # Método publico para modificar el valor de la latitud
     def setLatitud(self, valor):
         self.__latitud = valor
 
-    # Método para modificar el valor de la longitud 
+    # Método publico para modificar el valor de la longitud 
     def setLongitud(self, valor):
         self.__longitud = valor
         
-    # Método para modificar la descripción de la ubicación
+    # Método publico para modificar la descripción de la ubicación
     def setDescripcion(self, valor):
         self.__descripcion = valor
