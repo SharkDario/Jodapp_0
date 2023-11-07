39a213646f5328d2224bbd121e620b0cb077f068
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2023-11-07 13:56:02 -0300

    Update claseGrupo.py

--- api/claseGrupo.py
+++ api/claseGrupo.py
@@ -1,5 +1,10 @@
-## Define una clase llamada Grupo
+# Define una clase llamada Grupo
+# Es una clase abstracta que sirve como base para las clases Banda y Equipo
+# No se realiza instancia de la misma
 class Grupo():
+    # En el constructor se encuentra **kwargs 
+    # Es el parámetro que nos permite crear objetos a partir de pasarle un diccionario con todos los valores de los atributos
+    # Como la base de datos de Firebase guarda en formato diccionario, es una forma más sencilla de volver a crear los objetos
     def __init__(self, **kwargs):
         # Inicializa los atributos de la clase con valores proporcionados en kwargs
         self.__id = kwargs.get('id', "") # Obtiene el valor del ID, o una cadena vacía si no se proporciona
@@ -22,7 +27,9 @@ class Grupo():
     # Setter para establecer el ID del grupo
     def setID(self, valor):
         self.__id = valor
-
+        
+    # Los siguientes setters seran utilizados por las clase hijas de Grupo, donde se le pasaran directamente el tipo especifico
+    # Unicamente pueden actualizarse mediante su propio ID
     # Setter para establecer el nombre del grupo y actualizar en Firebase
     def setNombre(self, valor, firebase, tipo):
         if(firebase.editarAtributos(tipo, self.getID(), {'nombre': valor})):
@@ -41,7 +48,7 @@ class Grupo():
             datosInt += f"{integrante.mostrar()}\n"
         return datosInt
         
-    # Método para mostrar la información del grupo en formato de lista
+    # Método para mostrar la información del grupo en formato de cadena
     def mostrarLista(self):
         return f"Nombre:{self.__nombre}\t"
 
